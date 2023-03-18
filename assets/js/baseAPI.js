35fd2b0ebbe785e61d09d3ec3da5f07d5702e5e1
wl <wl01_26@126.com>
2023-03-18 18:19:16 +0800

    完成文章管理相关功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,6 +4,9 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    // options.url = 'http://127.0.0.1:3007' + options.url
+
+
 
     // 统一为有权限的接口，设置headers请求头
     if (options.url.indexOf('/my') !== -1) {
